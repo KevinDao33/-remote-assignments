36adf7f933e3849d8a2033b4e312be6338a4cb13
KevinDao <kevinph.chiou@gmail.com>
2022-01-21 19:11:36 +0800

    finish assignment 2 (give response of different message when putting  different params in the address bar)

--- Week3_Assignments_KevinChiou/app.js
+++ Week3_Assignments_KevinChiou/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cowsay = require("cowsay");
+const path = require("path");
 
 // app.set('view engine', 'pug');
 
@@ -14,9 +15,24 @@ app.get('/', (req, res) => {
 });
 
 app.get('/getData', (req, res) => {
-    res.render('index.html');
+    res.sendFile(path.join(__dirname, "index.html"));
+
+    let input = Number(req.query.integer);
+    if (Number.isInteger(input) === true && input >=0 && req.query.integer !== ""){
+        let result = 0;
+        for(let i=0; i<input+1; i++){
+        result += i;
+    }   console.log(result);
+        res.send('The result is : '+ result.toString());
+    } else if (req.query.integer == "" && req.query != "integer"){
+            console.log('Lack of Parameter');
+            res.send('Lack of Parameter');
+    } else if (req.query != "integer" && req.query.integer != null){
+            console.log(req.query.integer);
+            res.send("Wrong Parameter");
+    }
 });
 
-app.listen((3000), () => {
-    console.log("The server is running on port 3000.")
+app.listen((3001), () => {
+    console.log("The server is running on port 3001.")
 })
