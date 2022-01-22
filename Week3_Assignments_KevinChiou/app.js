852386ba60e07c2188f0498459d68ca78b78c17a
KevinDao <kevinph.chiou@gmail.com>
2022-01-23 03:47:19 +0800

    complete ajax/ fix some bugs in app.js/ remove result.html/ make the codes cleaner and easier to read

--- Week3_Assignments_KevinChiou/app.js
+++ Week3_Assignments_KevinChiou/app.js
@@ -3,6 +3,8 @@ const app = express();
 const cowsay = require("cowsay");
 const path = require("path");
 
+// middleware for /getData
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     console.log('hi, welcome to the server!');
@@ -14,35 +16,32 @@ app.get('/', (req, res) => {
     res.end();
 });
 
-// middleware for /getData
-app.use(express.static('public'));
-
 app.get('/getData', (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+     res.sendFile(path.join(__dirname, "./public/sum.html"));
 
     let input = req.query.integer;
     let isLackOfParam = !input || input === "";
     let inputNumber = Number(input);
     let propertyName = Object.getOwnPropertyNames(req.query)[0];
 
-    // the input is an integer
     if (Number.isInteger(inputNumber) && !isLackOfParam){
+        // the input is an integer
         let result = 0;
         for(let i=0; i<inputNumber+1; i++){
             result += i;
         }   
         console.log(result);
         res.send('The result is : ' + result.toString());
-    } 
-    // there's no input
-    else if (isLackOfParam && propertyName === "integer"){
-            console.log('Lack of Parameter');
-            res.send('Lack of Parameter');
-     } 
-     // there's input, but it's not a integer > 0
-    else if(!Number.isInteger(inputNumber) && !isLackOfParam){
-            console.log(req.query.integer);
-            res.send("Wrong Parameter");
+    } else if (isLackOfParam && propertyName === "integer"){
+        // there's no input
+        console.log('Lack of Parameter');
+        res.send('Lack of Parameter');
+     } else if(!Number.isInteger(inputNumber) && !isLackOfParam){
+        // there's input, but it's not a integer > 0
+        console.log(req.query.integer);
+        res.send("Wrong Parameter");
+    } else {
+        res.send("please ge to http://localhost:3000/sum.html");
     }
 });
 
