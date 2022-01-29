18cc7d094e229cb907cabfdcbcac3d8bf61b85d7
KevinDao <kevinph.chiou@gmail.com>
2022-01-30 02:08:09 +0800

    imporve logic in function, but there are still errors need to be fixed

--- Week4_Assignments_KevinChiou/Assignment2:CallbackFunctionandAdvancedHTMLDOM/app.js
+++ Week4_Assignments_KevinChiou/Assignment2:CallbackFunctionandAdvancedHTMLDOM/app.js
@@ -1,42 +1,29 @@
 let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-// const xhr = new XMLHttpRequest();
-// xhr.onreadystatechange = () => {
-//     if(xhr.readyState === 4){
-//         console.log(xhr.responseText);
-//         console.log(xhr.onreadystatechange);
-//     }
-// }
-// xhr.open('GET', 'https://appworks-school.github.io/Remote-Aassigiment-Data/products');
-// xhr.send();
-
-
 const ajax = (src, callback) => {
-    callback(src);
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', src);
+    xhr.send();
+    xhr.onreadystatechange = () =>{
+        if(xhr.readyState === 4){
+            callback(xhr.responseText);
+        }
+    }
 }
 
-const render = (data) => {
+const render = (finalData) => {
     const body = document.getElementById('root');
     let div = document.createElement('div');
-    let productData = document.createTextNode(data);
-    const productDiv = div.appendChild(productData);
+    let createText = document.createTextNode(finalData);
+    const productDiv = div.appendChild(createText);
     body.appendChild(productDiv);
 }
 
-ajax('https://appworks-school.github.io/Remote-Aassigiment-Data/products', () => {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
-        //this is where the render function goes (?)
-        if(xhr.readyState === 4){
-            const products = JSON.parse(xhr.responseText);
-            const productData = products.map(product => console.log(`Product : ${product.name}/ Price : $${product.price}/ Description : ${product.description}`));
-        }
-    }
-    xhr.open('GET', 'https://appworks-school.github.io/Remote-Aassigiment-Data/products');
-    xhr.send()
+ajax('https://appworks-school.github.io/Remote-Aassigiment-Data/products', (rawData) => {
+            const products = JSON.parse(rawData);
+            const productData = products.map(product => `Product : ${product.name}/ Price : $${product.price}/ Description : ${product.description}`);
+            const stringData = productData.toString();
+
+            render(stringData);
     }
 );
-
-// document. is not working, needs to be fixed
-// render('this is a div');
-
